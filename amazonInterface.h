a405b006459c20466b3f86337c2dd0e9a6fc4663
Greg Sexton <gregsexton@gmail.com>
2010-05-27 22:36:08 +0100

    Added Amazon access keys to preferences Xib.

---
+++ amazonInterface.h
@@ -0,0 +1,16 @@
+//
+//  amazonInterface.h
+//  books
+//
+//  Created by Greg on 26/05/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface amazonInterface : NSObject {
+
+}
+
+@end
---
+++ amazonInterface.m
@@ -0,0 +1,14 @@
+//
+//  amazonInterface.m
+//  books
+//
+//  Created by Greg on 26/05/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "amazonInterface.h"
+
+
+@implementation amazonInterface
+
+@end
