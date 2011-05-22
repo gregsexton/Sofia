d1d6605aeafc572df74f8f2a27743f21c6237987
Greg Sexton <gregsexton@gmail.com>
2011-05-22 23:25:55 +0100

    Ran headergen.

--- Version2To4EntityMigrationPolicy.m
+++ Version2To4EntityMigrationPolicy.m
@@ -1,9 +1,22 @@
 //
-//  Version2To4EntityMigrationPolicy.m
-//  books
+// Version2To4EntityMigrationPolicy.m
 //
-//  Created by Greg on 19/05/2011.
-//  Copyright 2011 Greg Sexton Software. All rights reserved.
+// Copyright 2011 Greg Sexton
+//
+// This file is part of Sofia.
+//
+// Sofia is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Sofia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sofia.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import "Version2To4EntityMigrationPolicy.h"
